d7d684079049d4b7e5824a14ca285594b1e0911b
huyt <huyt@huyUbuntu.myguest.virtualbox.org>
2024-12-01 17:50:52 -0800

    Assignment 5 part 2 first commit

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,8 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 3560271163c59fa9b32e6f5b77243d231620c2a5 #COMMIT VERSION NUMBER'
+#AESD_ASSIGNMENTS_VERSION = 3560271163c59fa9b32e6f5b77243d231620c2a5 #COMMIT VERSION NUMBER'
+AESD_ASSIGNMENTS_VERSION = 7243a55e99e9db623670d18fa296999bc21e876d #COMMIT VERSION NUMBER'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -28,6 +29,8 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer* $(TARGET_DIR)/usr/bin
 	
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop $(TARGET_DIR)/etc/init.d/S99aesdsocket
 endef
 
 $(eval $(generic-package))
